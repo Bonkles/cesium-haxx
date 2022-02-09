3433817a5ffb8cb958561a1c9251d3385ebd59e5
Benjamin Clark <clarkben@fb.com>
2022-02-09 13:49:23 -0500

    Set dependencies to load from a localhost base URL, add all assets, widgets, and workers required.

--- cesium.js
+++ cesium.js
@@ -1,6 +1,24 @@
 
+var options = {
+    animation: false,
+    baseLayerPicker: false,
+    fullscreenButton: false,
+    vrButton: false,
+    geocoder: false,
+    homeButton: false,
+    infoBox: false,
+    sceneModePicker: false,
+    selectionIndicator: false,
+    timeline: false,
+    navigationHelpButton: false,
+    navigationInstructionsInitiallyVisible: false,
+    skyBox: false,
+    skyAtmosphere: false
+};
+    window.CESIUM_BASE_URL = "http://127.0.0.1:5500/";
 
-var viewer = new Cesium.Viewer('cesiumContainer');
+var viewer = new Cesium.Viewer('cesiumContainer', options);
+
 
 
 
